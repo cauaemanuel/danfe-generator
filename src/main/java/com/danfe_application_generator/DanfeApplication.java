80d46910921c4f260367c0ec9104c25bcf4008c9
Araujo <cauaraujo@deloitte.com>
2025-09-09 14:23:29 -0300

    projeto iniciado

---
+++ src/main/java/com/danfe_application_generator/DanfeApplication.java
@@ -0,0 +1,13 @@
+package com.danfe_application_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanfeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danfe_application_generator/DanfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danfe_application_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
